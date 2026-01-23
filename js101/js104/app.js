aced7e5f9ef677be2ecfa0075e575d1ae2219f1e
rishuu <rishuu@rishuus-MacBook-Air.local>
2026-01-23 22:34:47 +0530

    do some more Question

---
+++ js101/js104/app.js
@@ -0,0 +1,71 @@
+// Question 1: Check Even or Odd
+// ❓ Task
+
+// Create a function checkEvenOdd that:
+
+// Takes a number
+
+// Returns a Promise
+
+// Resolves with "Even number" if the number is even
+
+// Rejects with "Odd number" if the number is odd
+
+function checkEvenOdd(num) {
+  return new Promise((resolve, reject) => {
+    if (num % 2 === 0) {
+      resolve("Even number");
+    } else {
+      reject("Odd number");
+    }
+  });
+}
+
+checkEvenOdd(6)
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
+
+//   Question 2: Delayed Square
+// ❓ Task
+
+// Create a function delayedSquare that:
+
+// Takes a number
+
+// Takes time in milliseconds
+
+// Returns a Promise
+
+// Uses setTimeout
+
+// // Resolves with the square of the number after the given time
+
+function delayedSquare(num, time) {
+  return new Promise((resolve, reject) => {
+    if (typeof num !== "number" || typeof time !== "number") {
+      reject("Invalid input");
+      return;
+    }
+
+    setTimeout(() => {
+      resolve(num * num);
+    }, time);
+  });
+}
+
+delayedSquare(5, 2000)
+  .then((result) => console.log(result)) // 25 (after 2s)
+  .catch((error) => console.log(error));
+
+//   Question 2: Delayed Square to async/await.
+
+async function run() {
+  try {
+    const result = await delayedSquare(5, 2000);
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+run();
