04a316ad53edb5df309729f0c66c6a3a0b468b31
Rishabh singh <rishabhsingh.block@gmail.com>
2026-02-01 23:42:53 +0530

    practice

---
+++ js101/js109/app.js
@@ -0,0 +1,15 @@
+// Task
+
+// Write a function called countVowels that:
+
+// Takes a string
+
+// Uses a for loop
+
+// Counts how many vowels (a, e, i, o, u) are in the string
+
+// Returns the count
+
+const string = "javaScript";
+
+function vowels(params) {}
