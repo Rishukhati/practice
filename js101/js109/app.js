21fa9d4c75056b7a8bd6660582687ecfe5d2db2d
Rishabh singh <rishabhsingh.block@gmail.com>
2026-02-03 22:14:47 +0530

    practice

--- js101/js109/app.js
+++ js101/js109/app.js
@@ -10,6 +10,40 @@
 
 // Returns the count
 
-const string = "javaScript";
+function vowels(params) {
+  let count = 0;
 
-function vowels(params) {}
+  for (let i = 0; i < params.length; i++) {
+    const char = params[i].toLowerCase();
+
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(vowels("javaScript")); // 3
+
+function wait(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Done waiting");
+    }, ms);
+  });
+}
+
+console.log("Start");
+
+setTimeout(() => {
+  console.log("Delayed");
+}, 1000);
+
+console.log("End");
